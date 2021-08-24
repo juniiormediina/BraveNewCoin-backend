89975648c769d538a09bf3430e6d19cd267d495b
Junior <juniiormediina@gmail.com>
2021-08-24 08:41:35 -0500

    si inicia proyecto y se hace configuracion base y se agregan las librerias que estaremos necesitando

---
+++ src/main/java/com/BraveNewCoinbackend/BraveNewCoinBackendApplication.java
@@ -0,0 +1,13 @@
+package com.BraveNewCoinbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BraveNewCoinBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BraveNewCoinBackendApplication.class, args);
+	}
+
+}
